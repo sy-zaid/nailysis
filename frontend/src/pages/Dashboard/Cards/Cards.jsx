fc30c8274e4f3423c12a992cde3d4c5c83c10544
abdullah1shah <shah.145abdullah1@gmail.com>
2024-12-11 21:29:29 +0500

    Created Dashboard!!

---
+++ frontend/src/pages/Dashboard/Cards/Cards.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./Cards.module.css";
+
+const Cards = (props) => {
+  const isFirstCard = props.heading === "Patients";
+
+  const cardStyle = {
+    backgroundColor: isFirstCard ? "#0067ff" : "#ffffff",
+    color: isFirstCard ? "#ffffff" : "#000000",
+  };
+
+  const percentStyle = {
+    backgroundColor: isFirstCard ? "#ffffff" : "#0067ff",
+  };
+
+  const percentColor = {
+    color: isFirstCard ? "#000000" : "#FFFFFF",
+  };
+
+  return (
+    <div className={styles.contain} style={cardStyle}>
+      <div className={styles.head}>
+        <div className={styles.imgDiv}>
+          <img src="stocks.png" alt="stocks icon" />
+        </div>
+        <h2 className={styles.title}>{props.heading}</h2>
+      </div>
+      <div className={styles.main}>
+        <h1 className={styles.count}>20,549</h1>
+        <div className={styles.percent} style={percentStyle}>
+          <h3>+15%</h3>
+        </div>
+      </div>
+      <div className={styles.foot}>
+        <p>
+          Data obtained from the past 7 days from 5,768 patients to 15,981
+          patients.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
