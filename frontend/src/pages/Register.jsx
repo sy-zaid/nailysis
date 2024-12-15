426b47466c4b10264248deceb20788c8669a91c0
sy-zaid <syedmzaid.99@gmail.com>
2024-12-15 13:47:59 +0500

    renamed signup.jsx to register.jsx

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "../components/Container/Container";
+import Form from "../components/Form/Form";
+
+const Register = () => {
+  return (
+    <Container>
+      <Form/>
+    </Container>
+  );
+};
+
+export default Register;
