8ec08deff97d760098114bfdf4c7b9e70bd30717
sy-zaid-blaze <syedmzaid.99@gmail.com>
2025-02-13 19:45:48 +0500

    switching device

---
+++ frontend/src/components/Popup/popup-appointment-edit.jsx
@@ -0,0 +1,312 @@
+import React, { useState, useEffect } from "react";
+import styles from "./popup-book-appointment.module.css";
+import Popup from "./Popup.jsx";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import usePatientData from "../../usePatientData.jsx";
+import { calculateAge } from "../../utils.js";
+
+const visitPurposes = [
+  "Consultation",
+  "Follow-up",
+  "Routine Checkup",
+  "Emergency Visit",
+  "Prescription Refill",
+];
+
+const PopupRescheduleAppointment = ({ onClose, appointmentDetails }) => {
+  const [popupTrigger, setPopupTrigger] = useState(true);
+  const navigate = useNavigate();
+  const [appointments, setAppointments] = useState([]);
+  const token = localStorage.getItem("access");
+  const curUserRole = localStorage.getItem("role");
+  const { data: curUser, isLoading, isError, error } = usePatientData(); // Fetch patient data
+  const [patient, setPatient] = useState([]); // Initialize patient state
+
+  useEffect(() => {
+    if (curUser && curUser.length > 0) {
+      setPatient([curUser[0].user, curUser[0]]); // Set patient data if available
+    } else {
+      console.log("No patient data available");
+    }
+  }, [curUser]);
+
+  const [formData, setFormData] = useState({
+    doctorId: appointmentDetails.doctorId || "",
+    appointmentDate: appointmentDetails.appointmentDate || "",
+    appointmentTime: appointmentDetails.appointmentTime || "",
+    appointmentType: appointmentDetails.appointmentType || "",
+    specialization: appointmentDetails.specialization || "",
+    fee: appointmentDetails.fee || "0.00",
+  });
+
+  const [specializations, setSpecializations] = useState([]);
+  const [doctors, setDoctors] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleDateTimeChange = (e) => {
+    const [date, time] = e.target.value.split("T");
+    setFormData((prev) => ({
+      ...prev,
+      appointmentDate: date,
+      appointmentTime: time || "",
+    }));
+  };
+
+  useEffect(() => {
+    const fetchSpecializations = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/api/doctors/", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setSpecializations(response.data);
+      } catch (error) {
+        console.error("Failed to fetch specializations", error);
+      }
+    };
+
+    fetchSpecializations();
+  }, [token]);
+
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      if (formData.specialization) {
+        try {
+          const response = await axios.get(
+            `http://127.0.0.1:8000/api/doctors/?specialization=${formData.specialization}`,
+            { headers: { Authorization: `Bearer ${token}` } }
+          );
+          const formattedDoctors = response.data.map((doc) => ({
+            id: doc.user.id,
+            name: `${doc.user.first_name} ${doc.user.last_name}`,
+          }));
+          setDoctors(formattedDoctors);
+        } catch (error) {
+          console.error("Failed to fetch doctors", error);
+        }
+      }
+    };
+
+    fetchDoctors();
+  }, [formData.specialization, token]);
+
+  const handleRescheduleAppointment = async (e) => {
+    e.preventDefault();
+
+    const appointmentData = {
+      doctor_id: formData.doctorId,
+      appointment_date: formData.appointmentDate,
+      appointment_time: formData.appointmentTime,
+      appointment_type: formData.appointmentType,
+      specialization: formData.specialization,
+      fee: formData.fee,
+      patient_name: patient?.first_name || "",
+      patient_age: patient?.age || "",
+      patient_gender: patient?.gender || "",
+      patient_phone: patient?.phone || "",
+      patient_email: patient?.email || "",
+    };
+
+    try {
+      const response = await axios.put(
+        `http://127.0.0.1:8000/api/doctor_appointments/${appointmentDetails.appointment_id}/reschedule_appointment/`,
+        appointmentData,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      alert("Appointment Rescheduled Successfully");
+      setAppointments([...appointments, response.data]);
+      navigate("");
+    } catch (error) {
+      alert("Failed to reschedule appointment");
+      alert("Failed to reschedule appointment");
+      console.error(error);
+    }
+  };
+
+  return (
+    <Popup trigger={popupTrigger} setTrigger={setPopupTrigger}>
+      <div className={styles.formContainer}>
+        <div className={styles.header}>
+          <h2>Reschedule Your Appointment</h2>
+        </div>
+
+        <h5 className={styles.subhead}>
+          Modify your appointment details and reschedule
+        </h5>
+        <hr />
+
+        <form onSubmit={(e) => e.preventDefault()}>
+          {/* Patient Information */}
+          <div className={styles.formSection}>
+            <h3>Patient Information</h3>
+            <div className={styles.formGroup}>
+              <div>
+                <label>Name</label>
+                <input
+                  type="text"
+                  name="patientId"
+                  value={formData.patientId}
+                  onChange={handleInputChange}
+                  placeholder={
+                    curUserRole === "patient"
+                      ? patient[0]?.first_name + " " + patient[0]?.last_name || ""
+                      : "Enter Full"
+                  }
+                  disabled={curUserRole === "patient"}
+                />
+              </div>
+              <div>
+                <label>Age</label>
+                <input
+                  type="text"
+                  name="age"
+                  value={formData.age}
+                  onChange={handleInputChange}
+                  placeholder={
+                    curUserRole === "patient"
+                      ? calculateAge(patient[1]?.date_of_birth) || ""
+                      : "Enter Age"
+                  }
+                  disabled={curUserRole === "patient"}
+                />
+              </div>
+              <div>
+                <label>Gender</label>
+                <input
+                  type="text"
+                  name="gender"
+                  value={formData.gender}
+                  onChange={handleInputChange}
+                  placeholder={
+                    curUserRole === "patient"
+                      ? patient[1]?.gender || ""
+                      : "Enter gender"
+                  }
+                  disabled={curUserRole === "patient"}
+                />
+              </div>
+              <div>
+                <label>Phone Number</label>
+                <input
+                  type="tel"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleInputChange}
+                  placeholder={
+                    curUserRole === "patient"
+                      ? patient[0]?.phone || ""
+                      : "Enter phone number"
+                  }
+                  disabled={curUserRole === "patient"}
+                />
+              </div>
+              <div>
+                <label>Email Address</label>
+                <input
+                  type="text"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  placeholder={
+                    curUserRole === "patient"
+                      ? patient[0]?.email || ""
+                      : "Enter email address"
+                  }
+                  disabled={curUserRole === "patient"}
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* Appointment Details */}
+          <div className={styles.formGroup}>
+            <label>Specialization</label>
+            <select
+              name="specialization"
+              value={formData.specialization}
+              onChange={handleInputChange}
+            >
+              <option value="">Select Specialization</option>
+              {specializations.map((spec, index) => (
+                <option key={index} value={spec}>
+                  {spec}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Doctor</label>
+            <select
+              name="doctorId"
+              value={formData.doctorId}
+              onChange={handleInputChange}
+            >
+              <option value="">Select Doctor</option>
+              {doctors.map((doctor) => (
+                <option key={doctor.id} value={doctor.id}>
+                  {doctor.name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Date & Time</label>
+            <input
+              type="datetime-local"
+              name="appointmentDateTime"
+              value={
+                formData.appointmentDate && formData.appointmentTime
+                  ? `${formData.appointmentDate}T${formData.appointmentTime}`
+                  : ""
+              }
+              onChange={handleDateTimeChange}
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Visit Purpose</label>
+            <select
+              name="appointmentType"
+              value={formData.appointmentType}
+              onChange={handleInputChange}
+            >
+              {visitPurposes.map((purpose, index) => (
+                <option key={index} value={purpose}>
+                  {purpose}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Fee</label>
+            <p className={styles.subHeading}>RS/- {formData.fee}</p>
+          </div>
+
+          <div className={styles.actions}>
+            <button className={styles.cancelButton} onClick={onClose}>
+              Cancel
+            </button>
+            <button
+              className={styles.confirmButton}
+              type="submit"
+              onClick={handleRescheduleAppointment}
+            >
+              Reschedule Appointment
+            </button>
+          </div>
+        </form>
+      </div>
+    </Popup>
+  );
+};
+
+export default PopupRescheduleAppointment;
--- frontend/src/pages/admin-clinic/upcoming-appointment-clinic-admin.jsx
+++ frontend/src/pages/admin-clinic/upcoming-appointment-clinic-admin.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { act, useEffect, useState } from "react";
 import styles from "../../components/CSS Files/PatientAppointment.module.css";
 import Navbar from "../../components/Dashboard/Navbar/Navbar";
 import PopupBookAppointment from "../../components/Popup/popup-book-appointment";
 import PopupAppointmentDetails from "../../components/Popup/popup-appointment-details";
+import PopupRescheduleAppointment from "../../components/Popup/popup-appointment-reschedule";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import api from "../../api";
@@ -11,30 +12,29 @@ const AppointmentClinicAdmin = () => {
   const navigate = useNavigate();
   const [appointments, setAppointments] = useState([]);
   const token = localStorage.getItem("access");
-
+  const [popupContent, setPopupContent] = useState();
+  const fetchAppointments = async () => {
+    try {
+      const response = await api.get(
+        `${import.meta.env.VITE_API_URL}/api/doctor_appointments/`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setAppointments(response.data);
+      console.log("Response from doctor appointment", response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
     if (!token) {
       console.log("No token found, Redirecting to login");
       navigate("/login");
       return;
     }
-
-    const fetchAppointments = async () => {
-      try {
-        const response = await api.get(
-          `${import.meta.env.VITE_API_URL}/api/doctor_appointments/`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        setAppointments(response.data);
-        console.log("Response from doctor appointment", response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
 
     fetchAppointments();
   }, [token, navigate]);
@@ -58,9 +58,67 @@ const AppointmentClinicAdmin = () => {
   const handleClosePopup = () => {
     setShowPopup(false); // Hide the popup when closing
   };
+
+  // Function to toggle the menu for a specific appointment
+  const toggleMenu = (appointmentId) => {
+    setMenuOpen(menuOpen === appointmentId ? null : appointmentId);
+  };
+
+  // Handle the action when an item is clicked in the menu
+  const handleActionClick = (action, appointmentId) => {
+    console.log(`Action: ${action} on Appointment ID: ${appointmentId}`);
+    setMenuOpen(null); // Close the menu after action
+
+    if (action === "Cancel") {
+      // POST the cancellation request
+      const handleCancellation = async (appointmentId, action) => {
+        try {
+          console.log(
+            "SUBMITTING APPOINTMENT ID FOR CANCELLING",
+            appointmentId
+          );
+          const response = await axios.post(
+            `${
+              import.meta.env.VITE_API_URL
+            }/api/doctor_appointments/${appointmentId}/cancel_appointment/`,
+            { action },
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("access")}`,
+              },
+            }
+          );
+          alert(response.data.message); // Notify of success
+
+          // Fetch updated appointments after cancellation
+          fetchAppointments();
+          // Optionally, refetch the cancellation requests list to reflect changes
+        } catch (err) {
+          console.log("Failed cancellation request.");
+        }
+      };
+      handleCancellation(appointmentId, action);
+    } else if (action === "Reschedule") {
+      setPopupContent(
+        <PopupRescheduleAppointment
+          appointmentDetails={appointmentId}
+          onClose={handleClosePopup}
+        />
+      );
+      setShowPopup(true);
+    } else if (action === "book_new_appointment") {
+      setPopupContent(<PopupBookAppointment onClose={handleClosePopup} />);
+      setShowPopup(true);
+    }
+
+    // Add logic for other actions like 'Edit' and 'Reschedule' if needed
+  };
+  const [menuOpen, setMenuOpen] = useState(null);
   return (
     <div className={styles.pageContainer}>
-      {showPopup && <PopupBookAppointment onClose={handleClosePopup} />}
+      {showPopup && popupContent}
+      {/* {showPopup && <PopupBookAppointment onClose={handleClosePopup} />} */}
+      {/* {showPopup && <PopupRescheduleAppointment onClose={handleClosePopup} />} */}
       <PopupAppointmentDetails></PopupAppointmentDetails>
 
       <div className={styles.pageTop}>
@@ -78,7 +136,10 @@ const AppointmentClinicAdmin = () => {
               <button className={styles.filterButton}>Cancelled</button>
               <p className={styles.statusSummary}>50 completed, 4 upcoming</p>
             </div>
-            <button className={styles.addButton} onClick={handleOpenPopup}>
+            <button
+              className={styles.addButton}
+              onClick={() => handleActionClick("book_new_appointment")}
+            >
               Book New Appointment
             </button>
           </div>
@@ -131,13 +192,58 @@ const AppointmentClinicAdmin = () => {
                       {row.appointment_date} {row.appointment_time}
                     </td>{" "}
                     {/* Date & Time */}
-                    <td>{row.status}</td> {/* Status */}
+                    <td className={getStatusClass(row.status)}>
+                      {row.status}
+                    </td>{" "}
+                    {/* Status */}
                     <td>{row.fee ? `PKR ${row.fee}` : "Not available"}</td>{" "}
                     {/* Fee */}
                     <td>{row.booking_date || "Not available"}</td>{" "}
                     {/* Booking Date */}
                     <td>{row.notes || "No additional notes"}</td>{" "}
                     {/* Additional Notes */}
+                    <td>
+                      <button
+                        onClick={() => toggleMenu(row.appointment_id)}
+                        className={styles.moreActionsBtn}
+                      >
+                        <img
+                          src="/icon-three-dots.png"
+                          alt="More Actions"
+                          className={styles.moreActionsIcon}
+                        />
+                      </button>
+                      {menuOpen === row.appointment_id && (
+                        <div className={styles.menu}>
+                          <ul>
+                            <li
+                              onClick={() =>
+                                handleActionClick("Edit", row.appointment_id)
+                              }
+                            >
+                              Edit
+                            </li>
+                            <li
+                              onClick={() => {
+                                handleActionClick("Cancel", row.appointment_id);
+                              }}
+                            >
+                              Cancel
+                            </li>
+                            <li
+                              onClick={() =>
+                                handleActionClick(
+                                  "Reschedule",
+                                  row
+                                )
+                              }
+                            >
+                              Reschedule
+                            </li>
+                          </ul>
+                        </div>
+                      )}
+                    </td>
                   </tr>
                 ))}
               </tbody>
