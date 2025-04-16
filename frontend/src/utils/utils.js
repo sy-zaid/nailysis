49ddf478bc2255e3f0572edf2f338b927c6389df
sy-zaid <syedmzaid.99@gmail.com>
2025-04-16 17:54:00 +0500

    pulled talha's branch

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -682,11 +682,10 @@ export const getFormattedCurrentTime = () => {
     .replace(",", ""); // optional: remove the comma if you want
 };
 
-
 export const getTodayDate = () => {
   const today = new Date();
   const yyyy = today.getFullYear();
-  const mm = String(today.getMonth() + 1).padStart(2, '0'); // Months start at 0!
-  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, "0"); // Months start at 0!
+  const dd = String(today.getDate()).padStart(2, "0");
   return `${yyyy}-${mm}-${dd}`;
 };
