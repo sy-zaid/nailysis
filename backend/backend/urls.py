40992bdc2c3d8395c946f266ee9ece166b8e2c94
sy-zaid <syedmzaid.99@gmail.com>
2025-05-30 12:12:40 +0500

    updated .env

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include, re_path
 
 from django.views.generic import TemplateView
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static  
 
 from api.views import CreateUserView, CustomerTokenObtainViewSerializer
 from rest_framework_simplejwt.views import TokenRefreshView
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import LoginForm from "../components/Form/LoginForm";
 import Picture from "../components/Picture/Picture";
 import styles from "../components/Form/Form.module.css";
 
-const Login = ({ LoginEndpoint = "/token/" }) => {
+const Login = ({ LoginEndpoint = "/token" }) => {
   console.log("API ADDRESS", import.meta.env.VITE_API_URL);
   return (
     <div className={styles.container}>
