7c9659e01c95da1996f6adc4f56a273fae11dcab
sy-zaid <syedmzaid.99@gmail.com>
2025-05-30 11:51:56 +0500

    updated end 10

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 import LoginForm from "../components/Form/LoginForm";
 import Picture from "../components/Picture/Picture";
-import styles from "../components/Form/Form.module.css"
+import styles from "../components/Form/Form.module.css";
 
 const Login = ({ LoginEndpoint = "/token/" }) => {
+  console.log("API ADDRESS", import.meta.env.VITE_API_URL);
   return (
     <div className={styles.container}>
       <Picture />
