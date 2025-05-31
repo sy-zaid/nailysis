eeb3d0af415f8658afaf8ebc2b70ec4ddfdca62d
sy-zaid <syedmzaid.99@gmail.com>
2025-05-31 12:44:26 +0500

    commented auth settings jwt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,9 +34,9 @@ ALLOWED_HOSTS = ["*"]
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+    # "DEFAULT_AUTHENTICATION_CLASSES": (
+    #     "rest_framework_simplejwt.authentication.JWTAuthentication",
+    # ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
