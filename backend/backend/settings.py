4d172d3a6474b282eb84f1078c48355f62c91971
sy-zaid <syedmzaid.99@gmail.com>
2025-05-31 14:53:25 +0500

    restructured3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",  # Changed from IsAuthenticated
     ],
     'DEFAULT_THROTTLE_CLASSES': [
         'model_service.throttles.RoleBasedRateThrottle',
