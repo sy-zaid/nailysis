62906ee162fd30090673436bbb25a4ebbcbce49a
sy-zaid <syedmzaid.99@gmail.com>
2025-05-31 14:26:09 +0500

    restructured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import React from "react";
+
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import Login from "./pages/Login";
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 import LoginForm from "../components/Form/LoginForm";
 import Picture from "../components/Picture/Picture";
-import styles from "../components/Form/Form.module.css";
+import styles from "../components/Form/Form.module.css"
 
-const Login = ({ LoginEndpoint = "/token" }) => {
-  console.log("API ADDRESS", import.meta.env.VITE_API_URL);
+const Login = ({ LoginEndpoint = "api/token/" }) => {
   return (
     <div className={styles.container}>
       <Picture />
