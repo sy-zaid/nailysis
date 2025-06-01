85a21cd7350c60704810988673012d292ef4b6e3
sy-zaid <syedmzaid.99@gmail.com>
2025-06-01 22:59:51 +0500

    trying csrf issue on models 114

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,7 +87,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
